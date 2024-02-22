003a678df02589204f66b8e84e28b50771745ba0
SvenHepkema <47423359+SvenHepkema@users.noreply.github.com>
2024-02-22 20:12:53 +0100

    Added clap and reading file contents into string

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::fs;
+
+use clap::Parser;
+
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    file_path: String,
+}
+
+fn read_file_contents(file_path: String) -> String {
+    let result = fs::read_to_string(file_path);
+    match result {
+        Ok(content) => return content,
+        Err(error) => panic!("Problem opening the file: {:?}", error),
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    let content = read_file_contents(args.file_path);
+    println!("{}", content);
 }
